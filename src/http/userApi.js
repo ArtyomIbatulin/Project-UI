8295c454c3d05ae563b11191b8e4715fffecd282
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2023-06-30 23:21:30 +0300

    fixed userApi

--- src/http/userApi.js
+++ src/http/userApi.js
@@ -1,10 +1,15 @@
 import { $host } from ".";
 
-const registration = async (login, password) => {
+export const signUp = async (login, password) => {
   const response = await $host.post("/api/v1/sign-up", {
     login,
     password,
     role: "user",
   });
+  return response;
+};
+
+export const signIn = async (login, password) => {
+  const response = await $host.post("/api/v1/sign-in", { login, password });
   return response;
 };
